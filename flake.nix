f0e2db7485bbb2b375360bbf49423c4da61a4b91
Bryan Lai <bryanlais@gmail.com>
2024-02-15 00:17:43 +0800

    Improve flake.nix, conforming to the output schema
    - Point nixpkgs to the "unstable" channel which enjoys binary caches,
      and bump flake.lock
    
    - Do not import nixpkgs; instead, use `legacyPackages.${system}` for
      better performance and composability.
    
    - Abstract pinix into a "package" and invoke with `pkgs.callPackage`,
      for future convenience.
    
    - Rewrite flake outputs such that it works well with the `nix build`
      and `nix develop` commands.
    
    See: https://nixos.org/manual/nix/unstable/command-ref/new-cli/nix#installables

--- flake.nix
+++ flake.nix
@@ -1,35 +1,53 @@
 {
-  description = "REST API for any Postgres database";
+  description = "A Pacman inspired frontend for Nix";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = { self, nixpkgs }:
-    with import nixpkgs { system = "x86_64-linux"; };
+    let
+      lib = nixpkgs.lib;
+      allSystems = lib.systems.flakeExposed;
+      forAllSystems = lib.genAttrs allSystems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pinix =
+            { rustPlatform
+            , installShellFiles
+            , lib
+            }:
+            rustPlatform.buildRustPackage {
+              pname = "pinix";
+              version = "0.1.0";
+              src = ./.;
+              nativeBuildInputs = [ installShellFiles ];
 
-    pkgs.rustPlatform.buildRustPackage rec {
-      pname = "pinix";
-      version = "0.1.0";
-      src = ./.;
-      nativeBuildInputs = [ pkgs.installShellFiles ];
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+                outputHashes = {
+                  "console-0.16.0" = "sha256-t5hydPT3BXJqPl8zKieaId3KUltEbRPh2xmZMhy8Ut0=";
+                  "indicatif-0.17.8" = "sha256-8xrGqDb6iUDhdvY937XFqC3GIZpq4tPMZAkKm218c/0=";
+                };
+              };
 
+              meta = with lib; {
+                description = "A Pacman inspired frontend for Nix";
+                homepage = "https://github.com/remi-dupre/pinix";
+                license = licenses.lgpl3;
+                mainProgram = "pinix";
+              };
 
-      cargoLock = {
-        lockFile = ./Cargo.lock;
-        outputHashes = {
-          "console-0.16.0" = "sha256-t5hydPT3BXJqPl8zKieaId3KUltEbRPh2xmZMhy8Ut0=";
-          "indicatif-0.17.8" = "sha256-8xrGqDb6iUDhdvY937XFqC3GIZpq4tPMZAkKm218c/0=";
-        };
-      };
-
-      meta = with pkgs.lib; {
-        description = "A Pacman inspired frontend for Nix";
-        homepage = "https://github.com/remi-dupre/pinix";
-        license = licenses.lgpl3;
-        mainProgram = "pinix";
-      };
-
-      cargoSha256 = "sha256-6hKbAL3a1t1mHNUvvi65e/BkFoJQmvpZQlU58csmol4=";
+              cargoSha256 = "sha256-6hKbAL3a1t1mHNUvvi65e/BkFoJQmvpZQlU58csmol4=";
+            };
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          pinix = pkgs.callPackage pinix { };
+          default = self.packages.${system}.pinix;
+        }
+      );
     };
 }
