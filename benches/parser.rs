c718c4458f3d04677c93bf03b1b6f96363b4ec1d
Rémi Dupré <remi@dupre.io>
2024-02-16 22:27:39 +0100

    parser benchmark

---
+++ benches/parser.rs
@@ -0,0 +1,42 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
+
+use divan::Bencher;
+use pinix::action::Action;
+
+fn main() {
+    divan::main();
+}
+
+fn load_example(example: &str) -> Vec<String> {
+    let path: PathBuf = ["examples", example].iter().collect();
+    let file = File::open(path).expect("could not open example");
+    let reader = BufReader::new(file);
+
+    reader
+        .lines()
+        .filter_map(|line| {
+            let line = line.expect("could not read line");
+            let msg = line.splitn(3, ' ').nth(2)?.strip_prefix("@nix ")?;
+            Some(msg.to_string())
+        })
+        .collect()
+}
+
+// Register a `fibonacci` function and benchmark it over multiple cases.
+#[divan::bench(args = ["nix-shell.rec", "nixos-rebuild.rec"])]
+fn parse(bencher: Bencher, example: &str) {
+    let example = load_example(example);
+    bencher
+        .with_inputs(|| &example)
+        .counter(divan::counter::ItemsCount::new(example.len()))
+        .bench_refs(|lines| {
+            lines
+                .iter()
+                .inspect(|line| {
+                    let _: Action = serde_json::from_str(line).expect("invalid line");
+                })
+                .count()
+        })
+}
